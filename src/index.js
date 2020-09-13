84a3a76720bb8747e03106eed6c8cb9c15be057f
GuilhermeFujita <fujitaguilherme@gmail.com>
2020-09-13 16:54:18 -0300

    Extract data and display in the console

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://www.google.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://busca.magazineluiza.com.br/busca?q=iphone');
 
-  await browser.close();
+  await page.evaluate(() => {
+    //Pegar todas as infos do navegador
+    const rawItems = document.querySelectorAll('.nm-product-item');
+    //Transformar em objetos JS
+    const itemsArray = [...rawItems]
+    //console.log('Raw', rawItems)
+    const products = [];
+    itemsArray.map(product => {
+      const finalProduct = {
+        name: product.innerText.substring(0, product.innerText.indexOf('R$')-3),
+        price: product.innerText.substring(product.innerText.indexOf('R$'))
+      }
+      products.push(finalProduct)
+    })
+
+    console.log('Products Array',products)
+    //Retornar
+  });
+
+  //await browser.close();
 })();
